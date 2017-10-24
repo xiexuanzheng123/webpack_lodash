11c1b641e01cec12a375752a1ca2d65f20999c1d
xiexuanzheng123 <349328984@qq.com>
2017-10-24 21:54:23 +0800

    build the enviroment

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: {
+        index: [
+            './app/array'
+        ]
+    },
+    output: {
+        path: path.join(__dirname, '/build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: [
+                    'babel-loader'
+                ]
+            },{
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    plugins: [
+        new HtmlwebpackPlugin({
+            title: 'lodash',
+            filename: 'build/lodash.html',
+            template: './index.html',
+            inject: 'body',
+            hash: true,
+            chunks: ['index']
+        })
+    ]
+}
