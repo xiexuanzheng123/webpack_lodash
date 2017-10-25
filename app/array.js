bb8c4bf761a2e3661ae9927d69c743ed817da67e
xiexuanzheng123 <349328984@qq.com>
2017-10-25 10:31:12 +0800

    'compact'

--- app/array.js
+++ app/array.js
@@ -1,4 +1,11 @@
 import _ from 'lodash';
 
+/**将array拆分为多个长度为size的块 */
 let chunkArr = ['a', 'b', 'c', 'd'];
-console.log(_.chunk(chunkArr, 2));
+console.log(_.chunk(chunkArr, 3));//[['a', 'b', 'c'], ['d']]
+
+/**去掉数组中为假的元素 
+ * false 0 "" undefined null NaN
+*/
+let  compactArr = [0, 1, false, 2, '', 3];
+console.log(_.compact(compactArr));
