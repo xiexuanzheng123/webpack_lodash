bee83431fa2a73f8439643585c04d8417cd6c5c8
xiexuanzheng123 <349328984@qq.com>
2017-10-25 17:35:12 +0800

    pull remove

--- app/array/array1.js
+++ app/array/array1.js
@@ -47,8 +47,69 @@ console.log(_.intersectionBy([2.1, 1.2], [4.3, 2.4]), Math.floor);//[2.1]
 console.log(_.intersectionBy([{'x': 1}], [{'x': 2}, {'x': 1}], 'x'));//[{'x': 1}]
 
 /**
- * intersectionWith
+ * intersectionWith 比较两个数组中的元素，返回第一个数组中的元素
  */
 var objects = [{'x': 1, 'y': 2}, {'x': 2, 'y': 1}];
 var others = [{'x': 1, 'y': 1}, {'x': 1, 'y': 2}];
 console.log(_.intersectionWith(objects, others, _.isEqual));//[{'x': 1, 'y': 2}]
+
+/**
+ * join
+ */
+console.log(_.join([1, 2, 3], '~'));//1~2~3
+
+/**
+ * last
+ */
+console.log(_.last([1, 2, 3]));//3
+
+/**
+ * lastIndexOf
+ */
+console.log(_.lastIndexOf([1, 2, 1, 2], 2));
+console.log(_.lastIndexOf([1, 2, 1, 2], 2, 2));
+
+/**
+ * nth
+ */
+console.log(_.nth(['a', 'b', 'c', 'd'], -1));//d
+
+/**
+ * pull 移除与给定元素相等的元素
+ */
+console.log(_.pull([1, 2, 3, 1, 2, 3], 2, 3));//[1, 1]
+
+/**
+ * pullAll与pull不同的是接受一个数组
+ */
+console.log(_.pullAll([1, 2, 3, 1, 2, 3], [2, 3]));//[1, 1]
+
+/**
+ * pullAllBy
+ */
+let array = [{'x': 1}, {'x': 2}, {'x': 3}, {'x': 1}];
+console.log(_.pullAllBy(array, [{'x': 1}, {'x': 3}], 'x'));// => [{ 'x': 2 }]
+
+/**
+ * pullAllWith
+ */
+let arrayWith = [{'x': 1, 'y': 2}, {'x': 3, 'y': 4}, {'x': 5, 'y': 6}];
+console.log(_.pullAllWith(arrayWith, [{'x': 3, 'y': 4}], _.isEqual));
+
+/**
+ * pullAt
+ */
+var arrayPullAt = [5, 10, 15, 20];
+var evens = _.pullAt(arrayPullAt, 1, 3);
+console.log(arrayPullAt);//[5, 15]
+console.log(evens);//[10, 20]
+
+/**
+ * remove
+ */
+let arrayRemove = [1, 2, 3, 4];
+let arr = _.remove(arrayRemove, function(n) {
+    return n % 2 === 0;
+})
+console.log(arrayRemove);//[1, 3]
+console.log(arr);//[2, 4]
