321e7adcf9bcbee81f86a26452b1306d1bb1f25e
xiexuanzheng123 <349328984@qq.com>
2017-10-25 10:45:16 +0800

    drop

--- app/array.js
+++ app/array.js
@@ -11,7 +11,14 @@ let  compactArr = [0, 1, false, 2, '', 3];
 console.log(_.compact(compactArr));
 
 /**
- * difference过滤数组
+ * difference过滤数组,去掉第一个数组中在第二个参数数组中包含的元素
  */
 let diffArr = [1, 2, 3];
 console.log(_.difference(diffArr, [4, 2]));
+
+/**
+ * drop 去掉数组中前N个元素
+ */
+let dropArr = [1, 2, 3];
+console.log('drop1: ', _.drop(dropArr, 1));//如果长度是1，则可以省略
+console.log('drop0: ', _.drop(dropArr, 0));
