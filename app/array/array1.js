48a49fdfa2f442dbd42c93171ac73cb119680191
xiexuanzheng123 <349328984@qq.com>
2017-10-25 16:47:09 +0800

    intersection indexOf

--- app/array/array1.js
+++ app/array/array1.js
@@ -18,3 +18,37 @@ console.log(_.flattenDepth([1, [2, [3, [4]], 5]], 2));//[1, 2, 3, Array(1), 5]
  * fromPairs返回一个由键值对构成的对象
  */
 console.log(_.fromPairs([['fred', 30], ['barney', 40]]));//{fred: 30, barney: 40}
+
+/**
+ * head和first一样
+ */
+
+ /**
+  * indexOf返回首次value在数组中被找到的索引值，
+  * 如果fromIndex为负值，则从数组尾端索引进行匹配,并且与值大小无关
+  */
+console.log(_.indexOf([1, 2, 1, 2], 2));//1
+console.log(_.indexOf([1, 2, 1, 2], 2, -2));//3
+
+/**
+ * initial去除最后一个元素
+ */
+console.log(_.initial([1, 2, 3]));//[1, 2]
+
+/**
+ * intersection获取多个数组的交集
+ */
+console.log(_.intersection([2, 1], [4, 2], [1, 2]));//[2]
+
+/**
+ * intersectionBy 将数组中的每个值变形后再比较，然后返回第一个数组中的元素
+ */
+console.log(_.intersectionBy([2.1, 1.2], [4.3, 2.4]), Math.floor);//[2.1]
+console.log(_.intersectionBy([{'x': 1}], [{'x': 2}, {'x': 1}], 'x'));//[{'x': 1}]
+
+/**
+ * intersectionWith
+ */
+var objects = [{'x': 1, 'y': 2}, {'x': 2, 'y': 1}];
+var others = [{'x': 1, 'y': 1}, {'x': 1, 'y': 2}];
+console.log(_.intersectionWith(objects, others, _.isEqual));//[{'x': 1, 'y': 2}]
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     devtool: 'cheap-module-eval-source-map',
     entry: {
         index: [
-            './app/array'
+            './app/array/array1'
         ]
     },
     output: {
