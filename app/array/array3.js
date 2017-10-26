a856ec7e5c3f220e38e5ea01373ed02990382e14
xiexuanzheng123 <349328984@qq.com>
2017-10-26 13:54:45 +0800

    xor

--- app/array/array3.js
+++ app/array/array3.js
@@ -47,3 +47,27 @@ var zipped = _.zip([1, 2], [10, 20], [100, 200]);
 //[[1, 10, 100], [2, 20, 200]]
 console.log(_.zipWith(zipped, _.add));
 //[3, 30, 300]
+
+/**
+ * without去除给定的元素
+ */
+console.log(_.without([2, 1, 2, 3], 1, 2));//[3]
+
+/**
+ * xor消除相同的元素
+ */
+console.log(_.xor([1, 2], [1, 3]));//[2, 3]
+console.log(_.xor([1, 2], [1, 2], [1, 2]));//[]
+console.log(_.xor([1, 2], [1, 2], [1]));//[]
+
+/**
+ * xorBy
+ */
+console.log(_.xorBy([2.1, 1.2], [2.3, 3.4], Math.floor));//[1.2, 3.4]
+
+/**
+ * xorWith
+ */
+var objects = [{'x': 1, 'y': 2}, {'x': 2, 'y': 1}];
+var others = [{'x': 1, 'y': 1}, {'x': 1, 'y': 2}];
+console.log(_.xorWith(objects, others, _.isEqual));
