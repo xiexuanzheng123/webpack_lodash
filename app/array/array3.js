4422aec7c6e95c3542ade200c964cb92dbdb8c68
xiexuanzheng123 <349328984@qq.com>
2017-10-26 11:39:24 +0800

    uniq

--- app/array/array3.js
+++ app/array/array3.js
@@ -13,6 +13,23 @@ console.log(_.unionBy([2.1], [1.2, 2.3], Math.floor));
 /**
  * unionWith
  */
-let objects = [{'x': 1, 'y': 2}, {'x': 2, 'y': 1}];
-let others = [{'x': 1, 'y': 1}, {'x': 1, 'y': 2}];
+var objects = [{'x': 1, 'y': 2}, {'x': 2, 'y': 1}];
+var others = [{'x': 1, 'y': 1}, {'x': 1, 'y': 2}];
 console.log(_.unionWith(objects, others, _.isEqual));
+
+/**
+ * uniq
+ */
+console.log(_.uniq([2, 1, 1, 2]));
+
+/**
+ * uniqBy
+ */
+console.log(_.unionBy([1.1, 1.5, 2.2, 2.4], Math.floor));
+
+/**
+ * uniqWith
+ */
+var objects = [{ 'x': 1, 'y': 2 }, { 'x': 2, 'y': 1 }, { 'x': 1, 'y': 2 }];
+console.log(_.uniqWith(objects, _.isEqual));
+
