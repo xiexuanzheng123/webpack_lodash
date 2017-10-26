723bf23fd609ca5f8e174c9dd87bd3b35e3b1d76
xiexuanzheng123 <349328984@qq.com>
2017-10-26 13:35:11 +0800

    zip unzip

--- app/array/array3.js
+++ app/array/array3.js
@@ -33,3 +33,17 @@ console.log(_.unionBy([1.1, 1.5, 2.2, 2.4], Math.floor));
 var objects = [{ 'x': 1, 'y': 2 }, { 'x': 2, 'y': 1 }, { 'x': 1, 'y': 2 }];
 console.log(_.uniqWith(objects, _.isEqual));
 
+/**
+ * unzip拆开数组
+ */
+var zipped = _.zip(['fred', 'barney'], [30, 40], [true, false]);
+//[['fred', 30, true], ['barney', 40, false]]
+console.log(_.unzip(zipped));
+
+/**
+ * unzipWith
+ */
+var zipped = _.zip([1, 2], [10, 20], [100, 200]);
+//[[1, 10, 100], [2, 20, 200]]
+console.log(_.zipWith(zipped, _.add));
+//[3, 30, 300]
